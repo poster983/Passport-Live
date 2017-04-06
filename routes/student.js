509d5c5656869923fc4e2d3f1fc8514862b55b15
Joseph Hassell <josephh2018@gmail.com>
2017-04-06 16:58:35 -0500

    Fixes

--- routes/student.js
+++ routes/student.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Student page. */
 router.get('/', function(req, res, next) {
-  res.render('student/index', { doc_Title: 'Passport-Student' });
+  res.render('student/index', { doc_Title: 'Passport-Student'});
 });
 
 module.exports = router;
