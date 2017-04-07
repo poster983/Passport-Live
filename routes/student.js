429a609a3884da15e5ed8026b9645369382688e7
Joseph Hassell <josephh2018@gmail.com>
2017-04-06 23:58:39 -0500

    Config implementation for pass groups

--- routes/student.js
+++ routes/student.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var config = require('config');
 var router = express.Router();
 
 /* GET Student page. */
 router.get('/', function(req, res, next) {
-  res.render('student/index', { doc_Title: 'Passport-Student'});
+	enabledPassGroups = config.get('passGroups.enabledPassGroups');
+	var passGroups = new Array();
+
+	for (var i = 0, len = enabledPassGroups.length; i < len; i++) {
+		/*
+		if(config.get('passGroups.' + enabledPassGroups[i] + '.viewType') == "select") {
+
+		}
+		*/
+		passGroups[i] = JSON.parse('{ "viewName":"' + config.get('passGroups.' + enabledPassGroups[i] + '.viewName') + '", "value": "' + config.get('passGroups.' + enabledPassGroups[i] + '.customGroupID') + '"}');
+		
+	}
+    res.render('student/index', { doc_Title: 'Passport-Student', passGroups: passGroups});
 });
 
 module.exports = router;
