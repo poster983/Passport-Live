41b51c645291549b88f34c5c263f03436d5c0d04
Joseph Hassell <josephh2018@gmail.com>
2017-05-11 23:11:08 -0500

    added stupid mini tests

--- test/test.js
+++ test/test.js
@@ -2,11 +2,28 @@ var request = require("request");
 var assert = require('assert');
 var server = require('../bin/www');
 var base_url = "http://localhost:3000/"
+
+
 describe("Server Test", function(){
 	describe("GET /", function() {
-		it("returns status code 200", function() {
+		it("returns status code 200 Continue", function() {
 			request.get(base_url, function(error, response, body) {
 				assert.equal(200, response.statusCode);
+				done();
+      });
+    });
+		it("returns status code 302 Found (Redirect)", function() {
+			request.get(base_url, function(error, response, body) {
+				assert.equal(302, response.statusCode);
+				done();
+      });
+    });
+  });
+	describe("GET /auth/login", function() {
+		it("returns status code 200 Continue", function() {
+			request.get(base_url+"auth/login", function(error, response, body) {
+				assert.equal(200, response.statusCode);
+				server.closeServer();
 				done();
       });
     });
