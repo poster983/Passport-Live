949240a99a8c6d14ebb31c92e4b2538b4ff8b9b8
Joseph Hassell <josephh2018@gmail.com>
2017-05-15 13:42:10 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -6,26 +6,22 @@ var base_url = "http://localhost:3000/"
 
 
 describe("Server Test", function(){
-	describe("GET /", function() { /*
+	describe("GET /", function() {
 		it("returns status code 200 Continue", function() {
 			request.get(base_url, function(error, response, body) {
 				assert.equal(103, response.statusCode);
 				done();
       });
-    });*//*
+    });
 		it("returns status code 302 Found (Redirect)", function() {
 			request.get(base_url, function(error, response, body) {
 				assert.equal(302, response.statusCode);
 				server.closeServer();
 				done();
       });
-    });*/
-		it("1+1== 2", function() {
-			assert.equal(2, 1+1);
-			server.closeServer();
-			
-		});
-  }); /*
+    });
+		
+  }); 
 	describe("GET /auth/login", function() {
 		it("returns status code 200 Continue", function() {
 			request.get(base_url+"auth/login", function(error, response, body) {
@@ -34,5 +30,5 @@ describe("Server Test", function(){
 				done();
       });
     });
-  }); */
+  }); 
 });
