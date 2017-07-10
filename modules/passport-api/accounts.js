67592cefbe7e7cb09278f53f3ae3131112568de8
Joseph Hassell <josephh2018@gmail.com>
2017-07-10 17:52:19 -0500

    [WIP]

--- modules/passport-api/accounts.js
+++ modules/passport-api/accounts.js
@@ -24,9 +24,11 @@ email: hi@josephhassell.com
 */
 var r = require('rethinkdb');
 var db = require('../../modules/db/index.js');
+const util = require('util')
 //DASHBOARDS//
 
 exports.getSchedulesForStudentDash = function(id, done) {
+    var promises = [];
     r.table("accounts").get(id).run(db.conn(), function(err, data) {
         if(err) {
             return done(err);
@@ -36,22 +38,56 @@ exports.getSchedulesForStudentDash = function(id, done) {
         for(var i = 0; i < periodKeys.length; i++) {
             var errs = null;
             var currPer = studentPeriodDatadata[periodKeys[i]];
-            if(currPer.hasOwnProperty("teacherID")) {
+            
+            if(currPer.hasOwnProperty("teacherID") && currPer.teacherID) {
                 //loop and get teacher 
-                
-                r.table("accounts").get(currPer.teacherID).pluck({'name': true, 'groupFields' : {'teacher': 'periodSchedule'}}).run(db.conn(), function(err, data) {
-                    if(err) {
-                        errs = err;
-                       
-                    }
-                   console.log(data)
-                });
+                console.log(currPer.teacherID)
+                //make promice
+                promises.push(r.table("accounts").get(currPer.teacherID).pluck({'name': true, 'groupFields': true}).run(db.conn()))
             }
 
-            if(i >= periodKeys.length -1 ) {
-                return done(errs, "hi")
-            }
         }
-        //return done(null, periodKeys)
+    //Resolve Promices//
+        Promise.all(promises).then(function(tDoc){
+            console.log("PeriodKeys")
+            console.log(periodKeys)
+            //for length of student
+            for(var i = 0; i < periodKeys.length; i++) {
+                //check if teacher has same periods as student 
+                if(teacherPeriodData.hasOwnProperty(periodKeys[i])) {
+
+                }
+            }
+            var teacherPeriodData = tDoc.groupFields.teacher.periodSchedule;
+            
+            console.log(teacherPeriodData);
+            //check if both have assigned the same periods 
+            if(teacherPeriodData.hasOwnProperty(periodKeys[i])) {
+                //console.log(periodKeys[i])
+                console.log(tDoc)
+                console.log(studentPeriodDatadata)
+                //add teacher data to student doc
+                //studentPeriodDatadata[periodKeys[i]] = teacherPeriodData[periodKeys[i]];
+            } else {
+                err = new Error("Teacher does not have \"" + periodKeys[i] + "\" period definned");
+                err.status = 412;
+                errs= err;
+            }
+
+        },function(err) {
+            if(err) {
+                return done(err);
+            }
+        })
     })
+}
+
+/*
+exports.getSchedulesForStudentDash = function(id, done) {
+    r.table("accounts").get(id).eqJoin(r.row('groupFields')('student')('periodSchedule')('a')('teacherID'), r.table("accounts")).zip().run(db.conn(), function(err, data) {
+        if(err) {
+            return done(err);
+        }
+        return done(null, data)
+        })*/
 }
