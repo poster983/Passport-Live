6b18b1e0be458149ee2df663cd0e75e6c54f836d
Joseph Hassell <josephh2018@gmail.com>
2017-08-10 11:53:34 -0500

    Api for perm key data

--- public/js/passport.js
+++ public/js/passport.js
@@ -369,3 +369,18 @@ function uuidv4() {
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   )
 }
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
