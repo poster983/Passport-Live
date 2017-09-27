8f403256e195e0a2a3d1f67a64a23f2c6005d3c3
Joseph Hassell <josephh2018@gmail.com>
2017-09-27 17:56:15 -0500

    Rate limiter works

--- modules/passport-utils/index.js
+++ modules/passport-utils/index.js
@@ -36,7 +36,7 @@ const db = require("../db/index.js");
 * @param {user} user - The user to clean.  The same user object found in the database 
 * @returns {user}
 */
-exprots.cleanUser = function(user){
+exports.cleanUser = function(user){
     if(user) {
         if(Array.isArray(user)){
             delete user[0].password;
@@ -101,18 +101,24 @@ exports.checkPeriod = function(period, done) {
     }
 }
 
+
+//brute prevention
+let store = new BruteRethinkdb(db.getDash(), {table: 'brute'});
+
 var failCallback = function (req, res, next, nextValidRequestDate) {
-    req.flash('error', "You've made too many failed attempts in a short period of time, please try again "+moment(nextValidRequestDate).fromNow());
-    res.redirect('/login'); // brute force protection triggered, send them back to the login page 
+    var err = new Error("You've made too many requests in a short period of time, please try again "+moment(nextValidRequestDate).fromNow());
+    err.status = 429;
+    return next(err);
+    //res.redirect('/login'); // brute force protection triggered, send them back to the login page 
 };
 
-var handleStoreError = handleStoreError: function (error) {
-    log.error(error); // log this error so we can figure out what went wrong 
+var handleStoreError = function (error) {
+    //console.error(error); // log this error so we can figure out what went wrong 
     // cause node to exit, hopefully restarting the process fixes the problem 
-    throw {
+    throw JSON.stringify({
         message: error.message,
         parent: error.parent
-    };
+    }, null, 4);
 }
 /**
     * Middleware to prevent brute force attacks   
@@ -122,9 +128,12 @@ exports.userBruteforce = new ExpressBrute(store, {
     freeRetries: 5,
     minWait: 5*60*1000, // 5 minutes 
     maxWait: 60*60*1000, // 1 hour, 
-    failCallback: failCallback,
+    failCallback: function (req, res, next, nextValidRequestDate) {
+        var err = new Error("You've made too many failed attempts in a short period of time, please try again "+moment(nextValidRequestDate).fromNow());
+        err.status = 429;
+        return next(err);
+    },
     handleStoreError: handleStoreError
-}
 });
 // No more than 1000 login attempts per day per IP 
 exports.globalBruteforce = new ExpressBrute(store, {
@@ -134,6 +143,27 @@ exports.globalBruteforce = new ExpressBrute(store, {
     minWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
     maxWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
     lifetime: 24*60*60, // 1 day (seconds not milliseconds) 
+    failCallback: failCallback,
+    handleStoreError: handleStoreError
+});
+
+exports.apiBruteforce = new ExpressBrute(store, {
+    freeRetries: 1000,
+    attachResetToRequest: false,
+    refreshTimeoutOnRequest: false,
+    minWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
+    maxWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
+    lifetime: 24*60*60, // 1 day (seconds not milliseconds) 
+    failCallback: failCallback,
+    handleStoreError: handleStoreError
+});
+
+exports.testBruteForse = new ExpressBrute(store, {
+    freeRetries: 3,
+    attachResetToRequest: true,
+    refreshTimeoutOnRequest: false,
+    minWait: 5*1000, // 1 day 1 hour (should never reach this wait time) 
+    maxWait: 1*60*1000, // 1 day 1 hour (should never reach this wait time) 
     failCallback: failCallback,
     handleStoreError: handleStoreError
 });
