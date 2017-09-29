c3dbbaf6c0ba64861b3456890f61afd79e0465c9
Joseph Hassell <josephh2018@gmail.com>
2017-09-28 22:36:58 -0500

    broken test

--- testx.js
+++ testx.js
@@ -3,7 +3,7 @@ convertExcel("/home/joseph/Desktop/passportImport/facultyhassell.xlsx", undefine
 	if(err) {
 		return console.error(err);
 	}
-	console.log(data)
+	//console.log(data, "raw DATA")
 	var mapRule = {
 		rule: {
 			name: {
@@ -14,7 +14,8 @@ convertExcel("/home/joseph/Desktop/passportImport/facultyhassell.xlsx", undefine
 			schoolID: "Faculty User Id",
 			email: "E-Mail",
 			userGroup: null,
-			isVerified: null
+			isVerified: null,
+			password: null
 		},
 		defaults: {
 			name: {
@@ -22,24 +23,122 @@ convertExcel("/home/joseph/Desktop/passportImport/facultyhassell.xlsx", undefine
 			},
 			userGroup: "teacher",
 			isVerified: true,
-			isArchived: false
+			isArchived: false,
+			password: "123"
 		}
 	}
+	/*
 	var opt = {
+		passwordRule
+
+	}*/
+
+	var mappedDataProm = data.filter(function(n) {
+		return new Promise(function(gRes, gRej) {
+			var returner = {};
+			returner.name = {};
+			if(!mapRule.rule.name || !mapRule.rule.name.first || !n[mapRule.rule.name.first]) {
+				if(mapRule.defaults.name.first) {
+					returner.name.first = mapRule.defaults.name.first;
+				} else {
+					return gRes(null);
+				}
+			} else {
+				returner.name.first = n[mapRule.rule.name.first];
+			}
+			if(!mapRule.rule.name || !mapRule.rule.name.last || !n[mapRule.rule.name.last]) {
+				if(mapRule.defaults.name.last) {
+					returner.name.last = mapRule.defaults.name.last;
+				} else {
+					return gRes(null);
+				}
+			} else {
+				returner.name.last = n[mapRule.rule.name.last];
+			}
+			if(!mapRule.rule.name || !mapRule.rule.name.salutation || !n[mapRule.rule.name.salutation]) {
+				if(mapRule.defaults.name.salutation) {
+					returner.name.salutation = mapRule.defaults.name.salutation;
+				} else {
+					return gRes(null);
+				}
+			} else {
+				returner.name.salutation = n[mapRule.rule.name.salutation];
+			}
+			if(!mapRule.rule.schoolID  || !n[mapRule.rule.schoolID]) {
+				if(mapRule.defaults.schoolID) {
+					returner.schoolID = mapRule.defaults.schoolID;
+				} else {
+					return gRes(null);
+				}
+			} else {
+				returner.schoolID = n[mapRule.rule.schoolID];
+			}
+			if(!mapRule.rule.email  || !n[mapRule.rule.email]) {
+				if(mapRule.defaults.email) {
+					returner.email = mapRule.defaults.email;
+				} else {
+					return gRes(null);
+				}
+			} else {
+				returner.email = n[mapRule.rule.email];
+			}
+			if(!mapRule.rule.userGroup  || !n[mapRule.rule.userGroup]) {
+				if(mapRule.defaults.userGroup) {
+					returner.userGroup = mapRule.defaults.userGroup;
+				} else {
+					return gRes(null);
+				}
+			} else {
+				returner.userGroup = n[mapRule.rule.userGroup];
+			}
+			if(!mapRule.rule.hasOwnProperty("isVerified")  || !n[mapRule.rule.userGroup]) {
+				if(mapRule.defaults.hasOwnProperty("isVerified")) {
+					returner.isVerified = mapRule.defaults.isVerified;
+				} else {
+					return gRes(null);
+				}
+			} else {
+				returner.isVerified = n[mapRule.rule.isVerified];
+			}
+			var passPromice = new Promise(function(resolve, reject) {
+				if(!mapRule.rule.hasOwnProperty("password")  || !n[mapRule.rule.password]) { 
+					if(mapRule.defaults.hasOwnProperty("password") && typeof mapRule.defaults.password == "function") {
+						mapRule.defaults.password(returner, function(err, password) {
+							if(err) {
+								reject();
+							} else {
+								returner.password = password;
+								resolve();
+							}
+						})
+					} else if(mapRule.defaults.hasOwnProperty("password") && typeof mapRule.defaults.password != "function") {
+						returner.password = mapRule.defaults.password;
+						resolve();
+					} else {
+						reject();
+					}
+				} else {
+					returner.password = n[mapRule.rule.password];
+					resolve();
+				}
+			})
+			//console.log(returner)
+			passPromice.then(function() {
+				//console.log("GOOOo")
+				return gRes(returner);
+			}).catch(function() {
+				return gRes(null);
+			})
+		});
 		
-
-	}
-
-	data.map(function(n) {
-		return {
-			name: {
-				first: n[mapRule.name.first],
-				last: n[mapRule.name.last],
-				salutation: n[mapRule.name.salutation]
-			},
-			schoolID: "Faculty User Id",
-			email: "E-Mail"
-		};
 	})
+
+	Promise.all(mappedDataProm).then(function(results) {
+		console.log(results, "this")
+	}).catch(function(err) {
+		return console.error(err);
+	})
+
+	//console.log(mappedData)
 	//process.exit();
 });
