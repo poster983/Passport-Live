b928b70f05acace30b343f064b900606bd2722cb
Joseph Hassell <josephh2018@gmail.com>
2017-12-08 19:45:30 -0600

    moved brute to own folder

--- modules/passport-utils/index.js
+++ modules/passport-utils/index.js
@@ -26,12 +26,10 @@ var jwt = require("jsonwebtoken");
 var shortid = require('shortid');
 var config = require("config");
 var moment = require("moment");
-const ExpressBrute = require('express-brute');
-const BruteRethinkdb = require('brute-rethinkdb')
-const db = require("../db/index.js");
 var uaParser = require('ua-parser-js');
 
 exports.typeCheck = require("./customTypeCheck.js");
+exports.rateLimit = require("./rateLimit.js");
 //console.log(exports.typeCheck)
 /**
 * Removes data like passwords and other sensitive info before sending it to the user 
@@ -240,97 +238,7 @@ exports.getBrowserSupport = function(userAgent) {
     })  
 }
 
-    
-//brute prevention
 
-
-var failCallback = function (req, res, next, nextValidRequestDate) {
-    var err = new Error("You've made too many requests in a short period of time, please try again "+moment(nextValidRequestDate).fromNow());
-    err.status = 429;
-    return next(err);
-    //res.redirect('/login'); // brute force protection triggered, send them back to the login page 
-};
-
-var handleStoreError = function (error) {
-    //console.error(error); // log this error so we can figure out what went wrong 
-    // cause node to exit, hopefully restarting the process fixes the problem 
-    throw JSON.stringify({
-        message: error.message,
-        parent: error.parent
-    }, null, 4);
-}
-
-//console.log(module.exports.generateSecureKey())
-
-/**
-    * Middleware to prevent brute force attacks when logging in
-    * @link module:js/utils
-    * @function loginBruteforce
-    */
-exports.loginBruteforce = new ExpressBrute(db.brute(), {
-    freeRetries: 5,
-    minWait: 1*60*1000, // 1 minute
-    maxWait: 60*60*1000, // 1 hour, 
-    failCallback: function (req, res, next, nextValidRequestDate) {
-        var err = new Error("You've made too many failed attempts in a short period of time, please try again "+moment(nextValidRequestDate).fromNow());
-        err.status = 429;
-        return next(err);
-    },
-    handleStoreError: handleStoreError
-});
-/**
-    * Middleware to prevent brute force attacks when requesing a password reset email 
-    * Retries: 3
-    * Remembers for 1 day
-    * @link module:js/utils
-    * @function emailPasswordResetBruteforce
-    */
-exports.emailPasswordResetBruteforce = new ExpressBrute(db.brute(), {
-    freeRetries: 3,
-    attachResetToRequest: false,
-    refreshTimeoutOnRequest: false,
-    minWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
-    maxWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
-    lifetime: 24*60*60, // 1 day (seconds not milliseconds) 
-    failCallback: function (req, res, next, nextValidRequestDate) {
-        var err = new Error("You've made too many requests in a short period of time, please try again "+moment(nextValidRequestDate).fromNow());
-        err.status = 429;
-        return next(err);
-    },
-    handleStoreError: handleStoreError
-});
-// No more than 1000 login attempts per day per IP 
-exports.globalBruteforce = new ExpressBrute(db.brute(), {
-    freeRetries: 1000,
-    attachResetToRequest: false,
-    refreshTimeoutOnRequest: false,
-    minWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
-    maxWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
-    lifetime: 24*60*60, // 1 day (seconds not milliseconds) 
-    failCallback: failCallback,
-    handleStoreError: handleStoreError
-});
-
-exports.publicApiBruteforce = new ExpressBrute(db.brute(), {
-    freeRetries: 30,
-    attachResetToRequest: false,
-    refreshTimeoutOnRequest: false,
-    minWait: 5*60*1000, // 1 hour (should never reach this wait time) 
-    maxWait: 60*60*1000, // 1 day 1 hour (should never reach this wait time) 
-    //lifetime: 24*60*60, // 1 day (seconds not milliseconds) 
-    failCallback: failCallback,
-    handleStoreError: handleStoreError
-});
-
-exports.testBruteForse = new ExpressBrute(db.brute(), {
-    freeRetries: 3,
-    attachResetToRequest: true,
-    refreshTimeoutOnRequest: false,
-    minWait: 5*1000, // 1 day 1 hour (should never reach this wait time) 
-    maxWait: 1*60*1000, // 1 day 1 hour (should never reach this wait time) 
-    failCallback: failCallback,
-    handleStoreError: handleStoreError
-});
 /**
 * A user object found in the database
 * @typedef {json} user
