4d66e7b679ed633354481bf1f660a034d3d06619
Joseph Hassell <josephh2018@gmail.com>
2018-01-09 14:11:44 -0600

    Logic fix

--- modules/passport-api/passes.js
+++ modules/passport-api/passes.js
@@ -106,7 +106,7 @@ exports.newPass = function (pass, options) {
             if (options.checkLimit !== false) {
                 exports.limitTally(pass.toPerson, pass.period, pass.date).then((limit) => {
                     console.log(limit)
-                    if(limit.tally > limit.passLimit && typeof limit.passLimit !== "number") {
+                    if(typeof limit.passLimit !== "number" || limit.tally < limit.passLimit) {
                         return limResolve("pending");
                     } else {
                         return limResolve("waitlisted");
