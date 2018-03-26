37f3ad6be056fb1cb9d2d526bc428c742281cadd
Joseph Hassell <josephh2018@gmail.com>
2018-03-26 17:50:53 -0500

    Removed test code

--- modules/webpack/entrypoints/student/index.js
+++ modules/webpack/entrypoints/student/index.js
@@ -24,7 +24,7 @@ email: hi@josephhassell.com
 document.createElement = Document.prototype.createElement;
 /** require webcomponents **/
 require("../../components/pass-list/pass-list.js");
-require("../../components/search-accounts/search-accounts.js");
+//require("../../components/search-accounts/search-accounts.js");
 
 /** require modules **/
 var utils = require("../../utils/index.js");
